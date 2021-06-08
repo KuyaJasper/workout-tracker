3a5704b9f041a2cd745b0ed98c82c2194f0fcee9
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-08 14:29:35 -0400

    set up workout model schema

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -4,22 +4,41 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
+const workoutSchema = new Schema({
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Workout type is required"
+      },
+      name: {
+        type: String,
+        required: "Workout needs a name"
+      },
+      duration: {
+        type: Number,
+        required: "Duration needs to be defined"
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      }
+  }
+  ],
+  day: {
     type: Date,
     default: Date.now
   }
 });
 
-const Workout = mongoose.model("Workout", transactionSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
